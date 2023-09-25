1bc3bebe139991c882ee4c70c2be512e3d11937c
EA - Kelly Dantas Da Silva Lima <dantas_kelly@educadventista.org>
2023-09-25 14:41:51 -0300

    Recap de node com express

---
+++ 1_INTRODUÇÃO/3_tarefa/index.js
@@ -0,0 +1,4 @@
+const n1 = 8
+const n2 = 89
+
+console.log(n1+n2)
---
+++ 2_EXPRESS/5_recap/index.js
@@ -0,0 +1,10 @@
+const express = require("express") //importar express
+const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Seja bem-vindo(a) ao meu servidor!")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
