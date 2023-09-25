07f6488dd8f3625aadae3a5076852b988f786ba8
EA - Kelly Dantas Da Silva Lima <dantas_kelly@educadventista.org>
2023-09-25 14:54:04 -0300

    Recapitulação de render html

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,8 +1,11 @@
 const express = require("express") //importar express
+const path = require("path")
+
 const app = express()
+const caminho = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Seja bem-vindo(a) ao meu servidor!")
+    resposta.sendFile(`${caminho}/index.html`)
 })
 
 app.listen(3000, () => {
